d09aca7d382bb925b048249f1b60373eceea1fee
dutchie027 <justin@227volts.com>
2020-12-26 22:16:17 -0500

    Update smokeping.sh

--- smokeping.sh
+++ smokeping.sh
@@ -16,6 +16,15 @@ read -p "What is your email address: "  userem
 resolvedIP=$(nslookup "$wsname" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
 [[ -z "$resolvedIP" ]] && echo "$wsname" lookup failure && exit || echo "$wsname" resolved to "$resolvedIP"
 
+# now lets check to make sure the email address entered is valud
+regex="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
+if [[ $userem =~ ${regex} ]]; then
+  echo "Valid emain...continuing"
+else
+  echo "$userem is not a valid email"
+  exit
+fi
+
 # TODO: Grab some user information as well as possibly
 # grabbing the hostname or even using wsname
 # and populating the default Targets
